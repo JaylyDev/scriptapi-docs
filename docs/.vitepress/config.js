3d89402e861034088f690d335988e3629887f3b4
Jayly <65847850+JaylyDev@users.noreply.github.com>
2023-02-09 19:40:31 +0000

    test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,9 @@
 import path from "path";
+import * as vitepress from "vitepress";
 
+/**
+ * @type {vitepress.UserConfig}
+ */
 export default {
   title: 'Script API References',
   description: 'Script API References',
