0285b94c917bca683e7e0fa95c7c57a705a774f9
Jayly <65847850+JaylyDev@users.noreply.github.com>
2023-08-06 09:06:30 +0800

    test

--- examples/@minecraft/server/World/prototype/afterEvents/sub_and_unsub.js
+++ examples/@minecraft/server/World/prototype/afterEvents/sub_and_unsub.js
@@ -1,4 +1,4 @@
 import { world } from "@minecraft/server";
 
-const callback = world.beforeEvents.itemUse.subscribe((event) => { /** event */});
-world.beforeEvents.itemUse.unsubscribe(callback);
+const callback = world.beforeEvents.itemUse.subscribe((event) => {}); // your event
+world.beforeEvents.itemUse.unsubscribe(callback); // unsubscribe
--- examples/@minecraft/server/World/prototype/beforeEvents/sub_and_unsub.js
+++ examples/@minecraft/server/World/prototype/beforeEvents/sub_and_unsub.js
@@ -1,4 +1,4 @@
 import { world } from "@minecraft/server";
 
-const callback = world.afterEvents.buttonPush.subscribe((event) => { /** event */});
-world.afterEvents.buttonPush.unsubscribe(callback);
+const callback = world.afterEvents.buttonPush.subscribe((event) => {}); // event
+world.afterEvents.buttonPush.unsubscribe(callback); // unsubscribe
