69c52ce7f6133542665154790e3764e7424792d3
WavePlayz <itsnikhilkumarniks@gmail.com>
2024-08-29 23:59:36 +0530

    Fix example isHardcore

--- examples/@minecraft/server/World/prototype/isHardcore/checkMode.js
+++ examples/@minecraft/server/World/prototype/isHardcore/checkMode.js
@@ -2,13 +2,11 @@
 
 import { world } from "@minecraft/server";
 
-// Subscribe to the `worldInitialize` event, which is triggered when the world is initialized
-world.afterEvents.worldInitialize.subscribe(() => {
+// Subscribe to the `playerSpawn` event, which is triggered when the world is initialized
+world.afterEvents.playerSpawn.subscribe(() => {
     // Check if the world is in hardcore mode
-    const isHardcoreMode = world.getDimension("overworld").isHardcore;
-
     // Notify players about the current mode
-    if (isHardcoreMode) {
+    if (world.isHardcore) {
         world.sendMessage("Welcome to Hardcore mode! Be careful, as death is permanent.");
     } else {
         world.sendMessage("This world is not in Hardcore mode. Play safely!");
